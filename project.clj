30ff137d2b583b2f2af9ebd998bdfa22e641a769
Casey Marshall <casey@metastatic.org>
2019-10-02 20:33:35 -0700

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.csm/konserve-ddb-s3 "0.1.0"
+(defproject com.github.csm/konserve-ddb-s3 "0.1.1-SNAPSHOT"
   :description "Konserve store atop DynamoDB and S3"
   :url "https://github.com/csm/konserve-ddb-s3"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
